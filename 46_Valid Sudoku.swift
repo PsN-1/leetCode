a6486cdfd1eabb76d15ddc60b25a91d5d3fcb097
Pedro Neto <pedro.santosnt@hotmail.com>
2021-11-11 23:01:50 -0300

    add a variation to solution #46

--- 46_Valid Sudoku.swift
+++ 46_Valid Sudoku.swift
@@ -1,28 +1,27 @@
+
 class Solution {
     func isValidSudoku(_ board: [[Character]]) -> Bool {
+        var isPossible = false
         for i in 0..<board.count {
             for j in 0..<board.count {
                 guard let col = Int(String(board[i][j])) else { continue }
                 if !chkAleatorio(i,j,col,board) {
                     return false
                 }
+                
             }
         }
         return true
     }
     
     func chkAleatorio(_ i: Int, _ j: Int, _ valorAleatorio: Int, _ col: [[Character]]) -> Bool {
-        for a in 0...8 {
-            if a == i { continue }
-            if col[a][j] == Character(String(valorAleatorio)) {
+        
+        for x in 0...8 {
+            if col[i][x] == Character(String(valorAleatorio)) && x != j ||
+            col[x][j] == Character(String(valorAleatorio)) && x != i {
                 return false
             }
-        }
-        for b in 0...8 {
-            if b == j { continue }
-            if col[i][b] == Character(String(valorAleatorio)) {
-                return false
-            }
+
         }
         for c in defQuadrante(i){
             for d in defQuadrante(j){
